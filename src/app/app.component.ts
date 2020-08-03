d01f4be44b70e7e05a7e96dbce02a6d760148047
wilsonmariano <mgw009@gmail.com>
2020-08-02 21:27:52 -0300

    Mejoro galeria de imágenes y agrego botón de whatsapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $;
+declare var SmoothScroll;
+declare var MenuSpy;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'singlePage';
+export class AppComponent implements OnInit {
+  scroll;
+
+
+
+  ngOnInit() {
+    this.initSmoothScroll();
+    this.initNavActiveScroll();
+  }
+
+  initSmoothScroll() {
+    this.scroll = new SmoothScroll('a[href*="#"]', {
+      speed: 1000,
+      speedAsDuration: true
+    });
+  }
+
+  initNavActiveScroll() {
+
+    var elm = document.querySelector('#main-header');
+    var ms = new MenuSpy(elm, {
+      // amount of space between your menu and the next section to be activated.
+      threshold     : 40,
+    });
+  }
 }
